de2a4de95a141fb65f6ad8bcb424abb6d5b8c9ee
Solomon <elmeriki@gmail.com>
2023-05-22 15:54:10 +0200

    new update

--- hospital/models.py
+++ hospital/models.py
@@ -81,7 +81,6 @@ class Bedcategory(models.Model):
     hospital = models.ForeignKey(User,on_delete=models.CASCADE,related_name="hospital_bed_category")
     categoryname =  models.CharField(max_length=200,blank=True,null=True,default="None")
     bednumber =  models.CharField(max_length=200,blank=True,null=True,default="None")
-    amount=  models.DecimalField(max_digits=11,decimal_places=0,default=0,blank=True,null=True)
     status = models.CharField(max_length=200,default=0,null=True,blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
